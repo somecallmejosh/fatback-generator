2e677a95e9eef86e37fd64f17348e3ab631602da
Joshua Briley <josh@thebrileys.com>
2015-11-11 19:02:47 -0500

    Remove jQuery dependency.

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,23 @@
 // Prompt user selection of fatback type
   // choices are limited to 'duple meter' and 'triple meter'
 
+// Set Constants
 var fatBackMax = 16;
 var timePatternMax = 25;
 var songMax = 91;
+var $audioPlayer = document.querySelector('.audio-player');
+var $resultA = document.querySelector('.result-a');
+var $resultA2 = document.querySelector('.result-a2');
+var $resultB = document.querySelector('.result-b');
+var $resultC = document.querySelector('.result-c');
+var $songNumber = document.querySelector('.song-number');
+var $recommendedPattern = document.querySelector('.recommended-pattern');
+var $recommendedPatternText = document.querySelector('.recommended-pattern-text');
+var $timingPattern = document.querySelector('.timing-pattern');
+var $fbSequence = document.querySelector('.fb-sequence');
+var $songRecommendation = document.querySelector('.song-recommendation');
+var $description = document.querySelector('.description');
+var $button = document.querySelector('button');
 
 function randomNumber (minNum, maxNum) {
   return Math.floor(Math.random() * (maxNum - 1) + minNum)
@@ -20,32 +34,40 @@ function updateAudio(sourceUrl){
   } else {
     setSpeed = '-slow.mp3'
   }
-  var audio = $('.audio-player');
-  $('.audio-player').attr("src", 'audio/' + sourceUrl + setSpeed); 
-  audio[0].pause();
-  audio[0].load();
+  $audioPlayer.setAttribute("src", 'audio/' + sourceUrl + setSpeed); 
+  $audioPlayer.pause();
+  $audioPlayer.load();
 }
 
 var generateFBRandoms = function() {
   var fatBackA = randomNumber(1, fatBackMax);
   var fatBackB = randomNumber(1, fatBackMax);
   var fatBackC = randomNumber(1, fatBackMax);
-  $('.result-a').text(fatBackA);
-  $('.result-b').text(fatBackB);
-  $('.result-c').text(fatBackC);
+  $resultA.textContent = fatBackA;
+  $resultA2.textContent = fatBackA;
+  $resultB.textContent = fatBackB;
+  $resultC.textContent = fatBackC;
 }
+
 var generateTimeAPattern = function() {
   var sectionTime = randomNumber(1, timePatternMax);
-  $('.recommended-pattern').text(sectionTime);
+  $recommendedPattern.textContent = sectionTime;
+  $recommendedPatternText.textContent = sectionTime;
 }
+
 var songRecommendation = function() {
   var randomSong = randomNumber(1, songMax);
   updateAudio(randomSong);
-  $('.song-number').text(randomSong);
+  $songNumber.textContent = randomSong;
 }
+
 var clearFBSequence = function() {
-  $('.timing-pattern, .fb-sequence, .song-recommendation, .description').removeClass("hidden");
+  $timingPattern.classList.remove("hidden");
+  $fbSequence.classList.remove("hidden");
+  $songRecommendation.classList.remove("hidden");
+  $description.classList.remove("hidden");
 }
+
 var displayFBSequence = function(){
   clearFBSequence();
   generateTimeAPattern();
@@ -53,6 +75,6 @@ var displayFBSequence = function(){
   songRecommendation();
 }
 
-$('button').on("click", displayFBSequence);
+$button.addEventListener("click", displayFBSequence);
 
   
